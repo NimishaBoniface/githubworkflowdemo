ab6d48fd30a299b3a0167d642bde759aaaccf6eb
nimishaboney1@gmail.com <nimishaboney1@gmail.com>
2025-06-03 11:54:02 +0100

    Initial Commit

---
+++ src/main/java/com/example/githubworkflowdemo/GithubworkflowdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubworkflowdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubworkflowdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubworkflowdemoApplication.class, args);
+	}
+
+}
